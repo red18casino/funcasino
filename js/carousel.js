2797cbf551c66c4cfef225cd6ab4acec0bcc1649
red18casino <38457484+red18casino@users.noreply.github.com>
2018-04-19 10:45:43 +0100

    Live site version1

---
+++ js/carousel.js
@@ -0,0 +1,14 @@
+
+  function changeSlide(n, no) {
+    showSlides(slideIndex[no] += n, no);
+  }
+
+  function showSlides(n, no) {
+    var x = document.getElementsByClassName(slideId[no]);
+    if (n > x.length) {slideIndex[no] = 1}
+    if (n < 1) {slideIndex[no] = x.length}
+    for (var i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    x[slideIndex[no]-1].style.display = "block";
+  }
